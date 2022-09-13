a5b034eb8abfc16df9c31a71216d241975c85678
jenkins <jenkins@jenkins.multitech.net>
2022-09-13 14:01:12 -0500

    xdot-library revision 4.1.4-22-g2f87583 and mbed-os revision mbed-os-6.16.0

--- plans/ChannelPlan_GLOBAL.cpp
+++ plans/ChannelPlan_GLOBAL.cpp
@@ -1137,7 +1137,7 @@ uint8_t ChannelPlan_GLOBAL::HandleRxParamSetup(const uint8_t* payload, uint8_t i
         status &= 0xFB; // Rx1DrOffset range KO
     }
 
-#if defined(ENABLE_LORAWAN_OPTIONAL_DATARATES)
+#if !defined(ENABLE_LORAWAN_OPTIONAL_DATARATES)
     // LCTT expects this Rx1Offset 6 & 7 to be rejected if TxDR is DR5 or greater
     if ((IsPlanAS923() && (drOffset >= 6 && (GetSettings()->Session.TxDatarate + (drOffset == 6) ? 1 : 2) > DR_5))) {
         logInfo("DR Offset KO");
