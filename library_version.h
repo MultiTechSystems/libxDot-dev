74adcd47d927fbd2ccfeb86f313481195aa3155b
jenkins <jenkins@jenkins.multitech.net>
2022-09-14 16:01:30 -0500

    xdot-library revision 4.1.4-26-g7a9ac3c and mbed-os revision mbed-os-6.16.0

--- library_version.h
+++ library_version.h
@@ -1,6 +1,6 @@
 #ifndef __LIBRARY_VERSION_H__ 
 #define __LIBRARY_VERSION_H__ 
-#define MDOT_VERSION "4.1.4-25-gc104874" 
+#define MDOT_VERSION "4.1.4-26-g7a9ac3c" 
 #define LW_VERSION "1.0.4" 
 #define RP_VERSION "1.0.3" 
 #endif 
