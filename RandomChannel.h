8fdaf554c96a9b3d2ae84425b51bf96138399879
jenkins <jenkins@jenkins.multitech.net>
2023-10-08 07:53:32 -0500

    xdot-library revision 4.1.4-1-g92957c6 and mbed-os revision mbed-os-6.8.0

--- RandomChannel.h
+++ RandomChannel.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <cstdint>
 
-namespace lora { 
+namespace lora {
     /** Generates uniform random channels on a per-subband basis. */
     class RandomChannel {
         public:
 
             /** Picks a random channel from the given list of enabled channels. When all channels in the list
              * have been used, the pool will reset and a new batch of random channels can be randomly selected.
-             * 
+             *
              * This method takes a list of enabled channels to minimize disruption of changes within the
-             * rest of the code base. The codebase calling this method has an array of enabled channels as a 
+             * rest of the code base. The codebase calling this method has an array of enabled channels as a
              * mask instead of keeping track of a subband. Passing a subband as an argument would require
              * a substantial refactor to the rest of the codebase.
              *
@@ -25,27 +25,36 @@ namespace lora {
 
             /** Gets the bitmask state containing the randomly used 125K channels
              * @return bitmask of the random channel distribution state - a '1' bit indicates the channel has
-             *         not been used for randomization, a '0' indicates the channel has been used for randomization.          
+             *         not been used for randomization, a '0' indicates the channel has been used for randomization.
              */
             uint64_t ChannelState125K() const;
 
             /** Gets the bitmask state containing the randomly used 500K channels
              * @return bitmask of the random channel distribution state - a '1' bit indicates the channel has
-             *         not been used for randomization, a '0' indicates the channel has been used for randomization.          
+             *         not been used for randomization, a '0' indicates the channel has been used for randomization.
              */
-            uint8_t ChannelState500K() const;
 
-            /** Sets the bitmask state caontaing the randomly used 125K channels 
+            uint32_t ChannelState500K() const;
+
+            /** Sets the bitmask state caontaing the randomly used 125K channels
              * @param bitmask of the random channel distribution state - a '1' bit indicates the channel has
-             *         not been used for randomization, a '0' indicates the channel has been used for randomization.          
+             *         not been used for randomization, a '0' indicates the channel has been used for randomization.
              */
             void ChannelState125K(uint64_t mask);
 
-            /** Sets the bitmask state caontaing the randomly used 500K channels 
+            /** Sets the bitmask state caontaing the randomly used 500K channels
              * @param bitmask of the random channel distribution state - a '1' bit indicates the channel has
-             *         not been used for randomization, a '0' indicates the channel has been used for randomization.          
+             *         not been used for randomization, a '0' indicates the channel has been used for randomization.
              */
-            void ChannelState500K(uint8_t mask);
+            void ChannelState500K(uint32_t mask);
+
+            /** Marks all 8 channels for a subband to unused. This helper method is called
+             *  typically whenever all the channels for a subband are pulled from a pool by NextChannel()
+             *  to allow pulling another round of random values
+             *
+             *  @param subband subband to mark unused
+             */
+            void MarkAllSubbandChannelsUnused(uint8_t subband);
 
         private:
 
@@ -67,33 +76,30 @@ namespace lora {
             /** Marks all 8 channels for a subband to unused. This helper method is called
              *  typically whenever all the channels for a subband are pulled from a pool by NextChannel()
              *  to allow pulling another round of random values
-             *  
+             *
              *  @param subband subband to mark unused
              */
             bool AllSubbandChannelsUsed(uint8_t subband) const;
 
-            /** Marks all 8 channels for a subband to unused. This helper method is called
-             *  typically whenever all the channels for a subband are pulled from a pool by NextChannel()
-             *  to allow pulling another round of random values
-             *  
-             *  @param subband subband to mark unused
-             */
-            void MarkAllSubbandChannelsUnused(uint8_t subband);
 
             /** Walks through the bitmask array and pulls the nth (index) unused and enabled channel.
              *  When called, this method will set the unusedChannel bit to a '0' to indicate the value
              *  has been pulled to retain uniform randomization.
-             *  
-             *  @param index number of active channels to pull from 
+             *
+             *  @param index number of active channels to pull from
              *  @param channel updated with the channel pulled, only valid when method returns true
              *  @return true if the channel could be pulled, else false
              */
             bool TakeActiveBit(uint8_t index, uint8_t *channel);
 
 
+#if CHANNEL_PLAN == CP_CN470
             static const uint8_t NUM_125K_SUBBANDS = 8;
-
+            static const uint8_t NUM_500K_SUBBANDS = 4;
+#else
+            static const uint8_t NUM_125K_SUBBANDS = 8;
             static const uint8_t NUM_500K_SUBBANDS = 1;
+#endif
 
             static const uint8_t NUM_SUBBANDS = NUM_125K_SUBBANDS + NUM_500K_SUBBANDS;
 
@@ -112,7 +118,7 @@ namespace lora {
              *  used for randomization
              */
             uint8_t _unusedChannels[NUM_SUBBANDS] = {0};
-            
+
             /** bitmask array for all 125K ands 500K channels to consider when pulling from randomization.
              */
             uint8_t _enabledChannels[NUM_SUBBANDS] = {0};
