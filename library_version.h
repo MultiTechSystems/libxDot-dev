ae9d5084876be3656080c82da19f3c3959e0bea4
Jenkins <Jenkins@KEILDM1.dc.multitech.prv>
2024-09-18 15:10:37 -0500

    xdot-library revision 4.1.30-8-gd257514 and mbed-os revision mbed-os-6.8.0

--- library_version.h
+++ library_version.h
@@ -1,6 +1,6 @@
 #ifndef __LIBRARY_VERSION_H__ 
 #define __LIBRARY_VERSION_H__ 
-#define MDOT_VERSION "4.1.30-7-g75bd6a0" 
+#define MDOT_VERSION "4.1.30-8-gd257514" 
 #define LW_VERSION "1.0.4" 
 #define RP_VERSION "1.0.3" 
 #endif 
